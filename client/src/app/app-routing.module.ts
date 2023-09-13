db3070d59e48cc362a009b01db3a0d467475425f
Karpurapu Amarnadh <126700935+Amarnadh2001@users.noreply.github.com>
2023-09-13 22:43:09 +0530

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { BookingsComponent } from './components/bookings/bookings.component';
@@ -12,6 +14,8 @@ import { MovieDetailsComponent } from './components/movie-details/movie-details.
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'theater-login', component: OwnerLoginComponent },
   { path: 'bookings', component: BookingsComponent },
   { path: 'movies', component: MoviesComponent },
