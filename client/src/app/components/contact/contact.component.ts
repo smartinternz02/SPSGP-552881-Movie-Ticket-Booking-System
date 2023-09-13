eddcac991aacdd71fc2b7fc6155a9e1e42f2e935
Karpurapu Amarnadh <126700935+Amarnadh2001@users.noreply.github.com>
2023-09-13 22:39:13 +0530

    Update contact.component.ts

--- client/src/app/components/contact/contact.component.ts
+++ client/src/app/components/contact/contact.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
+  submitted = false;
+  showSuccess = false;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    // You can perform any action here, such as sending the form data to a server.
+    // For this example, we'll simply show the success message.
+    this.showSuccess = true;
+
+    // Reset the form after submission if needed.
+    // this.contactForm.reset();
+
+    // Optionally, you can hide the success message after a certain time.
+    setTimeout(() => {
+      this.showSuccess = false;
+    }, 5000);
+  }
 }
